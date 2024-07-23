6f5c09f3ab788fa63647a51e637f242ef01aac28
isharanilangani <98374135+isharanilangani@users.noreply.github.com>
2024-07-23 10:05:01 +0530

    feat:SignIn Page

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import "./SignUpPage.css";
+import { useNavigate } from "react-router-dom";
 
-function SignInPage() {
+const SignInPage = () => {
+  const navigate = useNavigate();
+
+  const handleSignUpClick = () => {
+    navigate("/signup");
+  };
+
   return (
-    <div>
-      <h1>Task Management App</h1>
-      {/* Add your components here */}
+    <div className="signup-page d-flex">
+      <div className="left-side">
+        <img src="landing_image.jpg" alt="Background" className="img-fluid" />
+      </div>
+      <div className="right-side d-flex flex-column justify-content-center">
+        <h1>Sign In</h1>
+        <Form>
+          <Form.Group controlId="formEmail" className="form-group">
+            <Form.Label className="form-label">Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter your email"
+              className="form-control"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formPassword" className="form-group">
+            <Form.Label className="form-label">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter your password"
+              className="form-control"
+            />
+          </Form.Group>
+          
+          <div className="signup-button">
+          <Button variant="primary" type="submit" className="btn-primary">
+            Sign In
+          </Button>
+          </div>
+
+          <div className="or-separator">or</div>
+
+          <div className="social-buttons">
+          <Button variant="primary" type="button" className="btn-google">
+            Google
+          </Button>
+          <Button variant="primary" type="button" className="btn-facebook">
+            Facebook
+          </Button>
+          </div>
+
+          <p className="sign-in mt-3">
+            Don't you have an account?{" "}
+            <a href="/signup" onClick={handleSignUpClick}>
+              Sign Up
+            </a>
+          </p>
+        </Form>
+      </div>
     </div>
   );
-}
+};
 
 export default SignInPage;
